79da9c8ec5e4eff6490f8e1c301da8c7bdab8de5
osmant99 <ch.usman.tayyab@gmail.com>
2023-10-13 18:40:54 +0500

    height width adjustment issue and stop button is missing

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const start = () => {
+  let seconds = document.getElementById("seconds");
+  let minutes = document.getElementById("minutes");
+  let hours = document.getElementById("minutes");
+  // variables
+
+  let min = 0;
+  let sec = 0;
+  let hr = 0;
+
+  // seconds
+
+  let secInterval = setInterval(() => {
+    if (sec === 60) {
+      sec = 0;
+    }
+    sec = sec + 1;
+    seconds.innerHTML = sec;
+  }, 10 * 100);
+
+  // minutes
+
+  let minInterval = setInterval(() => {
+    if (min === 60) {
+      min = 0;
+    }
+    min = min + 1;
+    minutes.innerHTML = min;
+  }, 60 * 1000);
+
+  // hours
+
+  let hrsInterval = setInterval(() => {
+    hr = hr + 1;
+    hours.innerHTML = hr;
+  }, 36 * 100 * 1000);
+  const reset = () => {
+    clearInterval(secInterval);
+    clearInterval(minInterval);
+    clearInterval(hrsInterval);
+    document.getElementById("seconds").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+  };
+
+  document.getElementById("reset").addEventListener("click", reset);
+};
